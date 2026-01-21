212f78cc88a4f46597ab99ba6400bbf65dfc91fa
Tiwaran333 <caesarnarawit@gmail.com>
2026-01-21 19:00:16 +0700

    V1.1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 // index.js
 require('dotenv').config();
 const express = require('express');
@@ -14,3 +15,21 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+*/
+// index.js
+require('dotenv').config();
+const express = require('express');
+const { swaggerUi, specs } = require("./swagger");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/login", require("./routes/login"));
+
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// ❌ ลบ app.listen ออก
+// ✅ export app ให้ Vercel
+module.exports = app;
